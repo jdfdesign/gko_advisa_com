ea9a37b9abd09378f70761a609522d7f03dd69d0
jdfdesign <regis@joufdesign.com>
2012-12-06 11:59:29 -0400

    bump

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -11,49 +11,16 @@
 //= require jquery.throttledresize.js
 
 
-var $window,$body,contentApi, breakPoint, isTouch, currentTheme;
+var $window,$body, breakPoint, isTouch, currentTheme;
 
 var Site = {
 	
 	init: function() {
 		$window.on("throttledresize", Site.resize);
-		$('.nav li.dropdown').on('click.dropdown.data-api', function (e) {
-			if ($window.width() > breakPoint && !$body.hasClass('center')) {
-				e.preventDefault();
-				e.stopPropagation();
-				var url = $(this).find('a:first').attr("href");
-				window.location = "http://" + window.location.hostname + url;
-				return false;
-			}
-		})
 		Site.resize;
-		
-		$("#themes").css({
-			'position': 'fixed',
-			'top': 0,
-			'left': 0
-		}).find('a').css({
-			'display': 'block',
-			'background': 'black',
-			'color': 'white'
-		}).on('click', function(e) {
-			var $this = $(this),
-				theme = $this.attr('data-theme');
-				
-			if(typeof currentTheme != undefined) {
-				$body.removeClass(currentTheme);
-			}
-			if(typeof theme != undefined) {
-				$body.addClass(theme);
-			}	
-			
-			currentTheme = theme;
-		})
 	},
 	
 	resize: function() {
-		
-
 	}
 }
 
